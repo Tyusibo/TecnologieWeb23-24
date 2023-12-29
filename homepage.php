71b0552ae3d4aae259489987230e0bdf4fb84bf5
Rocco Manna <rocco.manna@itsvil.it>
2023-12-29 17:40:57 +0100

    l

--- homepage.php
+++ homepage.php
@@ -6,8 +6,35 @@
     <title>Gentlemen's Cut Homepage</title>
     <link rel="stylesheet" type="text/css" href="CSS/homepage.css">
 </head>
+
+<style> 
+    body{
+        margin: 0px;
+    }
+    .div_home{
+        background-image: url(img/homebackg.jpg);
+        height: 100vh;
+        background-size: cover;
+        display: flex;
+        
+    }
+    #welcome{
+        color: white;
+        font-size: 64px;
+    }
+
+</style>
+
 <body>
-    <iframe src="header.html" width="100%" height="200px" frameborder="0"></iframe>
+    
+
+    <section class="home" id="home" >
+        <div class="div_home">
+            <h1 id="welcome">Benvenuto da Gentlemn's Cut</h1>
+        </div>
+    </section>
+
+
     <iframe src="footer.html" width="100%" height="200px" frameborder="0"></iframe>
 </body>
 </html>

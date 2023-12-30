9329ad181d566a503949e1fd2788120a4e5cb839
Tyusibo <a.tudino@studenti.unisa.it>
2023-12-30 18:23:01 +0100

    aa

--- pagine_ausiliarie/esci.php
+++ pagine_ausiliarie/esci.php
@@ -1,1 +1,12 @@
-<a href="account.php" onclick=<?php session_destroy();?>>Esci</a>
+<?php
+session_start();
+// Invalida la sessione
+session_destroy();
+
+// Puoi anche eliminare specifiche variabili di sessione se necessario
+unset($_SESSION['username']);
+
+// Reindirizza alla pagina di accesso o a una pagina di conferma del logout
+header("Location: ../account.php");
+exit();
+?>

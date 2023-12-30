6357083b7706bf612b107b6bb7a9010441d855b8
Rocco Manna <rocco.manna@itsvil.it>
2023-12-30 12:18:33 +0100

    mappa in home page fix css

--- homepage.php
+++ homepage.php
@@ -4,125 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gentlemen's Cut Homepage</title>
-    <link rel="stylesheet" type="text/css" href="CSS/homepage.css">
+    <link rel="stylesheet" type="text/css" href="css/homepage.css">
+
+    <script async defer
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDhb5BVVHcIArfuJCj79LeG87fZyoPPnfQ&callback=initMap">
+    </script>
+    <script>
+        function initMap() {
+        // The location of Uluru
+        var uluru = {lat: 40.77248001098633, lng: 14.789327621459961};
+        // The map, centered at Uluru
+        var map = new google.maps.Map(
+            document.getElementById('map'), {zoom: 10, center: uluru});
+        // The marker, positioned at Uluru
+        var marker = new google.maps.Marker({position: uluru, map: map});
+        }
+
+        initMap();
+    </script>
+    
+
 </head>
-
-<style> 
-    body{
-        margin: 0px;
-    }
-    .div_home{
-        background-image: url(img/homebackg.jpg);
-        height: 100vh;
-        background-size: cover;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        z-index: 0;
-        
-    }
-    #welcome{
-        color: white;
-        font-size: 64px;
-        z-index: 2;
-    
-    }
-    .overlay{
-        top: 154px;
-        width: 100%;
-        height: 100vh;
-        position: absolute;
-        bottom: 0;
-        background-color: rgba(0, 0, 0, 0.4); /* Black see-through */
-        z-index: 1;
-    }
-    .btn{
-        z-index: 2;
-        align-items: center;
-        background-clip: padding-box;
-        background-color: #fa6400;
-        border: 1px solid transparent;
-        border-radius: .25rem;
-        box-shadow: rgba(0, 0, 0, 0.02) 0 1px 3px 0;
-        box-sizing: border-box;
-        color: #fff;
-        cursor: pointer;
-        display: inline-flex;
-        font-family: system-ui,-apple-system,system-ui,"Helvetica Neue",Helvetica,Arial,sans-serif;
-        font-size: 16px;
-        font-weight: 600;
-        justify-content: center;
-        line-height: 1.25;
-        margin: 0;
-        min-height: 3rem;
-        padding: calc(.875rem - 1px) calc(1.5rem - 1px);
-        position: relative;
-        text-decoration: none;
-        transition: all 250ms;
-        user-select: none;
-        -webkit-user-select: none;
-        touch-action: manipulation;
-        vertical-align: baseline;
-        width: auto;
-    }
-    .chiSiamo{
-        background-image: url(img/chiasiamobckg.jpg);
-        background-size: cover;
-        color: white;
-        height: 100vh;
-    }
-    .chiSiamo h1{
-        text-align: center;
-        margin: 0;
-    }
-    .chiSiamo .row{
-        display: flex;
-        justify-content: space-between;
-        align-items:  center;
-    }
-    .servizi {
-        background-color: orange;
-        display: grid;
-        grid-template-columns: 1fr 2fr; /* Due colonne di larghezza uguale */
-        gap: 20px; /* Spazio tra i servizi */
-        justify-content: center; /* Allinea la griglia al centro della section */
-    }
-
-    .servizi > div {
-        margin-left: 100px;
-        border: none; /* Stile di esempio per i bordi */
-        padding: 10px; /* Spaziatura interna di esempio */
-        max-width: 500px;
-        font-size: 20px;
-    }
-
-    /* Imposta le posizioni dei singoli servizi nella griglia */
-    .cut {
-    grid-column: 1; /* Prima colonna */
-    grid-row: 1; /* Prima riga */
-    }
-
-    .beradcut {
-    grid-column: 2; /* Seconda colonna */
-    grid-row: 1; /* Prima riga */
-    }
-
-    .buzzcut {
-    grid-column: 1; /* Prima colonna */
-    grid-row: 2; /* Seconda riga */
-    }
-
-    .shave {
-    grid-column: 2; /* Seconda colonna */
-    grid-row: 2; /* Seconda riga */
-    }
-
-    /* Aggiungi altre regole per gli altri servizi se necessario */
-
-    
-
-</style>
 
 <body>
 <iframe style="margin-bottom:-5px;" src="header.html" width="100%" height="154px" frameborder="0"></iframe>
@@ -138,10 +40,10 @@
     </section>
 
     <section class="chiSiamo" id="chiSiamo">
-        <h1>About us</h1>
+        <h1>Chi Siamo</h1>
         <div class="row">
             <div class="mapImg">
-                <img src="img/map.png" alt="mapImg">
+                <div id="map"></div>
             </div>
 
             <div class="content">
@@ -149,6 +51,7 @@
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
                    in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                   <a href="#" class="btn"><span>Contattaci</span></a>
             </div>
         </div>
     </section>

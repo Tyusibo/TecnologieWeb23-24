6f4e38e4ee95fb0cee973778b686d5f6078e48ae
AndreaPio <andreapiovicinanza@gmail.com>
2023-12-31 15:32:41 +0100

    header funzionante

--- script/StickyHeader.js
+++ script/StickyHeader.js
@@ -6,12 +6,11 @@ var sticky = header.offsetTop;
 var placeholder = document.createElement('div');
 placeholder.style.height = '100px';
 placeholder.style.backgroundColor = 'transparent';
+document.body.insertBefore(placeholder, document.body.firstChild);
 
 function myFunction() {
   if (window.scrollY > sticky) {
     header.style.backgroundColor = 'black';
-    header.classList.add("sticky");
-    document.body.insertBefore(placeholder, document.body.firstChild);
   } else {
     header.style.backgroundColor = 'transparent';
   }

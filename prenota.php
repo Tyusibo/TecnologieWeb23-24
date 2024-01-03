d6a10fcbd587a099e7c45226eed8f7dcb803d1b7
Tyusibo <a.tudino@studenti.unisa.it>
2024-01-03 21:19:59 +0100

    refactor

--- prenota.php
+++ prenota.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <?php
 session_start();
-$_SESSION['redirect']=null;   //tutte le pagine devono rendere redirect=null
+$_SESSION['redirect']=null;   //tutte le pagine devono rendere redirect=null e solo prenota.php deve cambiare quando serve
 if ($_SERVER["REQUEST_METHOD"] === 'GET') {  /*tramite form con metodo get capisco se reindirizzare al login o
     alla registrazione e imposto redirect per tornare a prenota dopo il login/la registrazione*/
     if (isset($_GET['accedi'])) {
@@ -13,7 +13,6 @@ if ($_SERVER["REQUEST_METHOD"] === 'GET') {  /*tramite form con metodo get capis
          header("Location: registrati.php");
  }    
 }
-
 ?>
 <html lang="it">
 <head>
@@ -22,53 +21,61 @@ if ($_SERVER["REQUEST_METHOD"] === 'GET') {  /*tramite form con metodo get capis
     <title>Gentlemen's Cut Prenota</title>
     <link rel="stylesheet" type="text/css" href="css/prenota.css">
 </head>
-
 <body>
     <?php require "header.html"; ?>
-    <div style="height: 100px"></div>  <!--lo stile per mostrare il contenuto dopo l'header-->
+    <div style="height: 100px;"></div>
     <?php
-    if(empty($_SESSION['username'])){  //se non loggato
-        ?>
+    if(empty($_SESSION['username'])){  //se non loggato ?>
         <p>Pagina riservata agli utenti autenticati. <br/> 
             <form action="<?php echo $_SERVER['PHP_SELF'] ?>"  method="GET">
 			    <label>Effettua l'<input type="submit" id="accedi" name="accedi" value="accesso"></form>oppure</label>
+            </br>
             <form action="<?php echo $_SERVER['PHP_SELF'] ?>"  method="GET">
-                <label><input type="submit" id="registrati" name="registrati" value="registrati">per continuare</p></label>
+                <label><input type="submit" id="registrati" name="registrati" value="registrati">per continuare</label>
             </form>
-        <?php } else {?>    
-        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST" enctype=“multipart/form-data”>
-            <label for="data">Seleziona una data:</label>
-            <input type="date" id="data" value="<?php echo $data ?>" name="data" min="<?php echo date('Y-m-d'); ?>" max="<?php echo date('Y-m-d', strtotime('+1 week')); ?>" required>
+        </p>
+        <?php } else /*se loggato*/if ($_SERVER["REQUEST_METHOD"] == "POST") {  //se hai effettuato una prenotazione ?>
+                <p>Prenotazione effettuata con successo!
+                    Clicca <a href="prenota.php">qui</a> per effettuarne un'altra
+                    oppure vai al tuo <a href="account.php">account</a> per gestire le tue prenotazioni.
+                </p>
+                <?php } else {  //se loggato ma non ha ancora effettuato una prenotazione
+                    echo "<p>Ciao $_SESSION[username] !";?>    
+                    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST" enctype=“multipart/form-data”>
+                        <label for="data">Seleziona una data:</label>
+                        <input type="date" id="data" value="<?php echo $data ?>" name="data" min="<?php echo date('Y-m-d'); ?>" max="<?php echo date('Y-m-d', strtotime('+1 week')); ?>" required>
 
-            <br>
+                        <br>
 
-            <label for="orario">Seleziona un orario:</label>
-            <select id="orario" name="orario" required>
-                <?php
-                    $orario_inizio = strtotime('09:00');
-                    $orario_fine = strtotime('18:00');
-                    $intervallo = 30 * 60; // 30 minuti in secondi
+                        <label for="orario">Seleziona un orario:</label>
+                        <select id="orario" name="orario" required>
+                            <?php
+                                $orario_inizio = strtotime('09:00');
+                                $orario_fine = strtotime('18:00');
+                                $intervallo = 30 * 60; // 30 minuti in secondi
 
-                    for ($ora = $orario_inizio; $ora <= $orario_fine; $ora += $intervallo) {
-                        $ora_selezionata = date('H:i', $ora);
+                                for ($ora = $orario_inizio; $ora <= $orario_fine; $ora += $intervallo) {
+                                    $ora_selezionata = date('H:i', $ora);
+                                    
+                                    // Verifica se la data selezionata non è domenica o lunedì
+                                    if (date('N', strtotime($_POST['data'])) >= 3) {
+                                        // Aggiungi solo gli orari validi
+                                        echo '<option value="' . $ora_selezionata . '">' . $ora_selezionata . '</option>';
+                                    }
+                                }
+                            ?>
+                        </select>
+
+                        <br>
+                        <label>Se hai qualche particolare preferenza, lasciaci un messaggio:
+                        <input type="text" id="message" name="message" value="Massimo 100 parole..."></label>
+                        </br>
+                        <label>Puoi anche inviarci un'immagine del taglio che desideri fare:
+                        <input type="file" id="file" name="file" value="File"></label>
+                        </br>
+                        <input type="submit" value="Invia">
+                    </form>
                         
-                        // Verifica se la data selezionata non è domenica o lunedì
-                        if (date('N', strtotime($_POST['data'])) >= 3) {
-                            // Aggiungi solo gli orari validi
-                            echo '<option value="' . $ora_selezionata . '">' . $ora_selezionata . '</option>';
-                        }
-                    }
-                ?>
-            </select>
-
-            <br>
-            <label>Se hai qualche particolare preferenza, lasciaci un messaggio:
-            <input type="text" id="message" name="message" value="Massimo 100 parole..."></label>
-            <label>Puoi anche inviarci un'immagine del taglio che desideri fare:
-            <input type="file" id="file" name="file" value="File"></label>
-            <input type="submit" value="Invia">
-        </form>
-            
         <?php } //parentesi dell'else ?>  
     
     <?php require "footer.html"; ?>

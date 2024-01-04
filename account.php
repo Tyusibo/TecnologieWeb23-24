5cdd5106f657b7b799f0189e493d2408fadc8a14
Tyusibo <a.tudino@studenti.unisa.it>
2024-01-04 15:35:44 +0100

    aa

--- account.php
+++ account.php
@@ -2,11 +2,11 @@
 <?php
 session_start();
 $_SESSION['redirect']=null;
-if (($_SERVER["REQUEST_METHOD"] === 'GET') && (isset($_GET['esci']))) { /*tramite form con metodo get capisco se è stata effettuata una richiesta di logout e la gestisco*/
+if (isset($_GET['esci'])) { /*Se è stata effettuata una richiesta di logout la gestisco*/
         session_destroy();
-        header("Location: homepage.php");  //per ricreare la sessione e inizializzare i valori
+        header("Location: homepage.php");  //rendirizzo a homepage, che sarà lui a ricreare la sessione e inizializzare i valori
         unset($_SESSION['username']);  //rimuovo gli attributi legati all'autenticazione
-        $x=false; //Se non faccio così dopo l'unset di $_SESSION['username'] mi farà sempre header("Location: autenticazione.php"); 
+        $redirect=false; //Se non faccio così dopo l'unset di $_SESSION['username'] mi farà sempre header("Location: autenticazione.php"); di riga 26
 }
 ?>
 <html lang="it">
@@ -15,30 +15,25 @@ if (($_SERVER["REQUEST_METHOD"] === 'GET') && (isset($_GET['esci']))) { /*tramit
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gentlemen's Cut Account</title>
     <link rel="stylesheet" type="text/css" href="css/account.css">
-    <script src="script/account.js" defer></script>
-    
-    <script src="https://kit.fontawesome.com/bdeddbfb58.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <?php require "header.php"; ?>
     <div class="container">
         <div class="whitebox">
-        <?php 
-        if((!(isset($_SESSION['username']))) && (!(isset($x)))){  //se non loggato  
-           header("Location: autenticazione.php"); 
-    }
-    else{  //se loggato
-        echo "<p>Benvenuto $_SESSION[username] !";?>
-        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="GET">
-			<input type="submit"  name="esci" id="esci" value="Esci">
-        </form>
-        <?php 
-    }
-    ?>
+            <?php 
+            if((!(isset($_SESSION['username']))) && (!(isset($redirect))))  //se non loggato e $redirect mi serve per capire se devo andare su 
+                //autenticazione o ignorarlo perchè devo andare su homepage
+            header("Location: autenticazione.php"); 
+            else{  //se loggato
+                echo "<p>Benvenuto $_SESSION[username] !";?>
+                <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="GET">  <!--form per gestire il log out-->
+                    <input type="submit" name="esci" id="esci" value="Esci">
+                </form>
+                <?php 
+            }
+            ?>
         </div>  <!--Devo chiudere i 2 div-->
     </div>
-    
-    <?php require "footer.html"; ?>
-       
+    <?php require "footer.html"; ?> 
 </body>
 </html>

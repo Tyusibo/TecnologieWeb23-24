73cd6aa1a6d4d162e47af66ac83f4fb3f575a09e
Tyusibo <a.tudino@studenti.unisa.it>
2024-01-05 17:45:18 +0100

    account

--- account.php
+++ account.php
@@ -5,6 +5,8 @@ $_SESSION['redirect']=null;  //valore di default che mi fa capire che in autenti
 $_SESSION['change']=false;  //valori di default che mi fa capire che in autenticazione.php devo mostrare la parte del 
 //login e non quella della registrazione(true)
 //entrambe le variabili non vengono alterate in autenticazione.php ma vengono inizializzate in ogni altra pagina
+if(!(isset($_SESSION['username'])))  //se non loggato
+    header("Location: autenticazione.php"); 
 ?>
 <html lang="it">
 <head>
@@ -17,16 +19,16 @@ $_SESSION['change']=false;  //valori di default che mi fa capire che in autentic
     <?php require "header.php"; ?>
     <div class="container">
         <div class="whitebox">
-            <?php 
-            if(!(isset($_SESSION['username'])))  //se non loggato
-            header("Location: autenticazione.php"); 
-            else{  //se loggato
-                echo "<p>Benvenuto $_SESSION[username] !";?>
-                <p>Premi <button id="esci">qui</button> per uscire</p>
-                </form>
-                <?php 
-            }
-            ?>
+        <section id="lista">
+            <?php echo "<p>Benvenuto $_SESSION[username] !";?>
+            <p id="dati">I miei dati</p> 
+            <p id="prenotazioni">Le mie prenotazioni</p>  
+            <p id="preferenze">Le mie preferenze</p> 
+            <p id="esci">Premi <button id="esci">qui</button> per uscire</p>            
+        </section>
+        <div id="contenuti">
+        Varia in base a quello premuto
+        </div>
         </div>  <!--Devo chiudere i 2 div-->
     </div>
     <?php require "footer.php"; ?> 

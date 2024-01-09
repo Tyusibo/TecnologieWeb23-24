a6c22ccb772a0652df82b256ba6eec6c8d7bea85
Tyusibo <a.tudino@studenti.unisa.it>
2024-01-09 22:11:25 +0100

    a

--- database/registrati.php
+++ database/registrati.php
@@ -25,14 +25,14 @@
         require "connectionString.php";
         $db = pg_connect($connection_string) or die('Impossibile connetersi al database: ' . pg_last_error());
         $sql = "INSERT INTO public.utenti(nome, cognome, numero, username, password_hash,pref_1,pref_2,pref_3)
-                        VALUES($1, $2, $3, $4, $5); ";
-        $nome = pg_escape_literal($nome);
+                        VALUES($1, $2, $3, $4, $5, NULL, NULL, NULL); ";
+        /*$nome = pg_escape_literal($nome);
         $cognome = pg_escape_literal($cognome);
         $numero = pg_escape_literal($numero);
         $username = pg_escape_literal($username);
         $pwd = pg_escape_literal($pwd);
-        $hashed_pwd = password_hash($pwd, PASSWORD_DEFAULT);
-        $ret=pg_query_params($db, $sql,array($nome, $cognome, $numero, $username, $hashed_pwd,"","",""));
+        $hashed_pwd = password_hash($pwd, PASSWORD_DEFAULT);*/
+        $ret=pg_query_params($db, $sql,array($nome, $cognome, $numero, $username, $hashed_pwd));
         if(!$ret) {
             echo "ERRORE QUERY: " . pg_last_error($db);
             pg_close($db);

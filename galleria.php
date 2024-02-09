3ea45bbaf79aeee68d65dd116cb44f477cafefd6
fraspin02 <155062426+fraspin02@users.noreply.github.com>
2024-02-09 16:42:42 +0100

    gallery

--- galleria.php
+++ galleria.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <?php
+//buzz cut //french crop //curtains cut //Side Part Cut //MOHAWK cut
 session_start(); 
 $_SESSION['redirect']=null;  
 ?>
@@ -9,10 +10,51 @@ $_SESSION['redirect']=null;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gentlemen's Cut Galleria</title>
     <link rel="stylesheet" type="text/css" href="css/galleria.css">
+    <script src="script/galleria.js" crossorigin="anonymous"></script> 
 </head>
 <body>
     <?php require "header.php"; ?>
     <div style="height: 100px"></div>
+
+    <h1>I NOSTRI TAGLI</h1>
+
+    <div class="gallery">
+        <div class="gal-1">
+            <div class="galint">
+                <button class="category active" id="all" onclick="dispAll()">ALL</button>
+                <button class="category"  id="buzzcat" onclick="buzz()">BUZZ CUT</button>
+                <button class="category"  id="frenchcat" onclick="french()">FRENCH CROP</button>
+                <button class="category"  id="curtainscat" onclick="curtains()">CURTAINS</button>
+                <button class="category"  id="sidecat" onclick="side()">SIDE PART</button>
+                <button class="category"  id="mohawkcat" onclick="mohawk()">MOHAWK</button>
+            </div>
+        </div>
+        <div class="gal-2">
+            <?php 
+            
+                for($i = 1; $i<6; $i++)
+                    echo '<img  class="galimg buzz" id="buzz" src="img/gallery/img'.$i.'.jpg" >' ;
+                for($i = 6; $i<11; $i++)
+                    echo '<img  class="galimg french" id="french" src="img/gallery/img'.$i.'.jpg" >' ;
+                for($i = 11; $i<16; $i++)
+                    echo '<img  class="galimg curtains" id="curtains" src="img/gallery/img'.$i.'.jpg" >' ;
+                for($i = 16; $i<21; $i++)
+                    echo '<img  class="galimg side" id="side" src="img/gallery/img'.$i.'.jpg" >' ;
+                for($i = 21; $i<26; $i++)
+                    echo '<img  class="galimg mohawk" id="mohawk" src="img/gallery/img'.$i.'.jpg" >' ;
+
+            ?>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
     <?php require "footer.php"; ?>
 </body>
 </html>
---
+++ script/galleria.js
@@ -0,0 +1,75 @@
+function buzz() {
+    all();
+    var Imgs = document.querySelectorAll('.galimg.buzz');
+    Imgs.forEach(function(img) {
+        img.style.display = 'block';
+    });
+    var button = document.getElementById("buzzcat");
+    active(button);
+}
+
+function french() {
+    all();
+    var Imgs = document.querySelectorAll('.galimg.french');
+    Imgs.forEach(function(img) {
+        img.style.display = 'block';
+    });
+    var button = document.getElementById("frenchcat");
+    active(button);
+}
+function curtains(){
+    all();
+    var Imgs = document.querySelectorAll('.galimg.curtains');
+    Imgs.forEach(function(img) {
+        img.style.display = 'block';
+    });
+    var button = document.getElementById("curtainscat");
+    active(button);
+}
+function side(){
+    all();
+    var Imgs = document.querySelectorAll('.galimg.side');
+    Imgs.forEach(function(img) {
+        img.style.display = 'block';
+    });
+    var button = document.getElementById("sidecat");
+    active(button);
+}
+function mohawk(){
+    all();
+    var Imgs = document.querySelectorAll('.galimg.mohawk');
+    Imgs.forEach(function(img) {
+        img.style.display = 'block';
+    });
+    var button = document.getElementById("mohawkcat");
+    active(button);
+}
+
+function dispAll(){
+    all();
+    var allImgs = document.querySelectorAll('.galimg');
+    allImgs.forEach(function(img) {
+        img.style.display = 'block';
+    });
+    var button = document.getElementById("all");
+    active(button);
+}
+
+function all(){
+    var allImgs = document.querySelectorAll('.galimg');
+    allImgs.forEach(function(img) {
+        img.style.display = 'none';
+    });
+
+    var active = document.querySelectorAll('.category');
+    active.forEach(function(button) {
+        button.classList.remove("active");
+    });
+}
+
+function active(button){
+    button.classList.add("active");
+}
+
+
+
